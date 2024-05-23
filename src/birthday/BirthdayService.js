4bdfd760a484973c249d5f30e5f962930cf7bf71
Luzhania Cespedes Arancibia <luzhaniacesar@gmail.com>
2024-05-23 10:13:02 -0400

    extract read from file logic to another function

--- src/birthday/BirthdayService.js
+++ src/birthday/BirthdayService.js
@@ -1,22 +1,13 @@
 import fs from "fs";
 import path from "path";
 import { Employee } from "./Employee";
+import { get } from "http";
 
 export class BirthdayService {
   constructor() {}
 
   sendGreetings(ourDate, fileName, smtpUrl, smtpPort, transport) {
-    const data = fs.readFileSync(
-      path.resolve(__dirname, `${fileName}`), //`../${fileName}`),
-      "UTF-8"
-    );
-
-    // split the contents by new line
-    const lines = data.split(/\r?\n/);
-    lines.shift();
-    const employees = lines
-      .map((line) => this.createEmployeeFromLine(line))
-      .filter((employee) => employee.isBirthday(ourDate));
+    let employees = this.getEmployeesByBirthday(ourDate, fileName);
 
     employees.forEach((employee) => {
       const message = {
@@ -29,6 +20,21 @@ export class BirthdayService {
       };
       transport.sendMail(message);
     });
+  }
+
+  getEmployeesByBirthday(ourDate, fileName) {
+    const data = fs.readFileSync(
+      path.resolve(__dirname, `${fileName}`), //`../${fileName}`),
+      "UTF-8"
+    );
+
+    // split the contents by new line
+    const lines = data.split(/\r?\n/);
+    lines.shift();
+    const employees = lines
+      .map((line) => this.createEmployeeFromLine(line))
+      .filter((employee) => employee.isBirthday(ourDate));
+    return employees;
   }
 
   createEmployeeFromLine(line) {
